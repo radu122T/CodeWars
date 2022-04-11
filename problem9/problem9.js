30519eb8d792278dc81aa2cc5aa44356d741cd12
radu122T <radu.cudalb@yahoo.ro>
2022-04-11 17:00:15 +0300

    problem9

---
+++ problem9/problem9.js
@@ -0,0 +1,34 @@
+// Some numbers have funny properties. For example:
+
+// 89 --> 8¹ + 9² = 89 * 1
+
+// 695 --> 6² + 9³ + 5⁴= 1390 = 695 * 2
+
+// 46288 --> 4³ + 6⁴+ 2⁵ + 8⁶ + 8⁷ = 2360688 = 46288 * 51
+
+// Given a positive integer n written as abcd... (a, b, c, d... being digits) and a positive integer p
+
+// we want to find a positive integer k, if it exists, such that the sum of the digits of n taken to the successive powers of p is equal to k * n.
+// In other words:
+
+// Is there an integer k such as : (a ^ p + b ^ (p+1) + c ^(p+2) + d ^ (p+3) + ...) = n * k
+
+// If it is the case we will return k, if not return -1.
+
+// Note: n and p will always be given as strictly positive integers.
+
+function digPow(n,p){
+    let sumOfPowers = 0
+    let listWithDigits = String(n).split("")
+    console.log(listWithDigits)
+    for (let i=0; i<listWithDigits.length; i++)
+        sumOfPowers += Math.pow(listWithDigits[i],(p+i))
+    if (sumOfPowers % n ===0) 
+        return sumOfPowers / n 
+    else 
+        return -1
+}
+
+console.log(digPow(695,2))
+
+
