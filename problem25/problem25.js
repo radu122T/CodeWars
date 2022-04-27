7038a6f859e53d41f2480fb6be142f764a7d9f2e
radu122T <radu.cudalb@yahoo.ro>
2022-04-27 16:59:45 +0300

    problem25

--- problem25/problem25.js
+++ problem25/problem25.js
@@ -5,3 +5,15 @@
 // Return the direction you will face after the turn.
 
 // Examples
+
+function direction(facing, turn) {
+    let directions = ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW'];
+    let rotate = Math.floor(turn / 45);
+   
+    let indexOfDirection = directions.indexOf(facing);
+    let facingFinalIndex = (indexOfDirection + rotate) % directions.length
+    if (facingFinalIndex < 0) {
+      return directions[directions.length - Math.abs(facingFinalIndex)]
+    }
+    return directions[facingFinalIndex]
+  }
