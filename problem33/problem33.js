d4beda876957f1ac1a8b1bf16ef32d40dae76a48
radu122T <radu.cudalb@yahoo.ro>
2022-05-02 15:36:04 +0300

    problem42

--- problem33/problem33.js
+++ problem33/problem33.js
@@ -24,12 +24,4 @@ function sameCase(a, b){
     return 0
   }
 
-console.log(sameCase('C', 'B'))
-console.log(sameCase('b', 'a'))
-console.log(sameCase('d', 'd'))
-console.log(sameCase('A', 's'))
-
-console.log(sameCase('c', 'B'))
-console.log(sameCase('b', 'Z'))
-console.log(sameCase('\t', 'Z'))
-console.log(sameCase(':', '4'))
+  
