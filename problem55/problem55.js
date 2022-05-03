10400e42bc40ac51448b46a3c13f07ce31139c21
radu122T <radu.cudalb@yahoo.ro>
2022-05-03 16:30:51 +0300

    problem55

---
+++ problem55/problem55.js
@@ -0,0 +1,8 @@
+// Simple, remove the spaces from the string, then return the resultant string.
+
+
+function noSpace(x){
+    return x.split(" ").join("")
+}
+
+console.log((noSpace('8 j 8   mBliB8g  imjB8B8  jl  B'), '8j8mBliB8gimjB8B8jlB'))
