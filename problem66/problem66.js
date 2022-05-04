6fd779c8726ccb4fa21a08262e774c6edbabc1e5
radu122T <radu.cudalb@yahoo.ro>
2022-05-04 17:57:01 +0300

    problem66

---
+++ problem66/problem66.js
@@ -0,0 +1,18 @@
+// You are given two sorted arrays that both only contain integers. Your task is to find a way to merge them into a single one, sorted in asc order. Complete the function mergeArrays(arr1, arr2), where arr1 and arr2 are the original sorted arrays.
+
+// You don't need to worry about validation, since arr1 and arr2 must be arrays with 0 or more Integers. If both arr1 and arr2 are empty, then just return an empty array.
+
+// Note: arr1 and arr2 may be sorted in different orders. Also arr1 and arr2 may have same integers. Remove duplicated in the returned result.
+
+
+function mergeArrays(arr1, arr2) {
+    let k = []
+    arr1.push(...arr2)
+    console.log(arr1)
+    let newSet = new Set(arr1.sort((a, b) => a-b))
+    
+    return Array.from(newSet)
+}
+
+
+console.log(mergeArrays([2,34,5,4],[3,5,61,2]))
